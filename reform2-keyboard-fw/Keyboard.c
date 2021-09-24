0c828d9b2cfcbe51fd29e03f59c02b1d7fb6fdb0
Lukas F. Hartmann <lukas@mntre.com>
2021-09-24 20:30:19 +0200

    kbd: bump revision

--- reform2-keyboard-fw/Keyboard.c
+++ reform2-keyboard-fw/Keyboard.c
@@ -41,7 +41,7 @@
 #include <stdlib.h>
 #include <avr/sleep.h>
 
-#define KBD_FW_REV "R1 20210815"
+#define KBD_FW_REV "R1 20210924"
 //#define KBD_VARIANT_STANDALONE
 #define KBD_VARIANT_QWERTY_US
 //#define KBD_VARIANT_NEO2
