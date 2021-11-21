57d4ba9ece582e3f1ae8b58b0052d920c30deaa5
Lukas F. Hartmann <lukas@mntre.com>
2021-11-21 20:14:04 +0100

    keyboard: WIP some temp. example code for various hacky things

--- reform2-keyboard-fw/Keyboard.c
+++ reform2-keyboard-fw/Keyboard.c
@@ -681,10 +681,21 @@ void reset_keyboard_state(void) {
   last_meta_key = 0;
 }
 
+/*void game(void) {
+  while (game_running) {
+    process_keyboard(0, NULL);
+    game_paint();
+    Delay_MS(100);
+  }
+}*/
+
+// usb_report_mode: if you pass 0, you can leave KeyboardReport NULL
 void process_keyboard(char usb_report_mode, USB_KeyboardReport_Data_t* KeyboardReport) {
   // how many keys are pressed this round
   uint8_t total_pressed = 0;
   uint8_t used_key_codes = 0;
+
+  int special_pgup = 0;
 
   // pull ROWs low one after the other
   for (int y=0; y<ROWS; y++) {
@@ -745,6 +756,8 @@ void process_keyboard(char usb_report_mode, USB_KeyboardReport_Data_t* KeyboardR
           if (!active_meta_mode && !last_meta_key) {
             enter_meta_mode();
           }
+        } else if (keycode == HID_KEYBOARD_SC_PAGE_UP) {
+          special_pgup = 1;
         } else {
           if (active_meta_mode) {
             // not holding the same key?
@@ -784,6 +797,14 @@ void process_keyboard(char usb_report_mode, USB_KeyboardReport_Data_t* KeyboardR
     case 3: output_high(PORTD, 7); break;
     case 4: output_high(PORTD, 6); break;
     case 5: output_high(PORTD, 4); break;
+    }
+  }
+
+  // example special casing code
+  if (special_pgup) {
+    for (int i=0; i<used_key_codes; i++) {
+      int k = KeyboardReport->KeyCode[i];
+      KeyboardReport->KeyCode[i] = k+1;
     }
   }
 
@@ -1066,5 +1087,9 @@ void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t* const HIDI
   }
   else if (data[0]=='B' && data[1]=='M' && data[2]=='A' && data[3]=='P') {
     matrix_render_direct(data+4);
+  }
+  else if (data[0]=='J' && data[1]=='U' && data[2]=='M' && data[3]=='P') {
+    void (*trampoline)(void) = (void (*)(void))&data[4];
+    trampoline();
   }
 }
--- reform2-keyboard-fw/ssd1306.h
+++ reform2-keyboard-fw/ssd1306.h
@@ -92,5 +92,4 @@ void matrix_write(struct CharacterMatrix *matrix, const char *data);
 void matrix_write_ln(struct CharacterMatrix *matrix, const char *data);
 void matrix_write_P(struct CharacterMatrix *matrix, const char *data);
 void matrix_render(struct CharacterMatrix *matrix);
-
-//bool process_record_gfx(uint16_t keycode, keyrecord_t *record);
+void matrix_render_direct(uint8_t* bitmap);
